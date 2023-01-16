98b6b5b62382d01d8a5eca6f62785735e14f49b3
Mina Yossry <mina.yossry4@gmail.com>
2023-01-16 20:56:34 +0200

    add Librarian

---
+++ Library/Librarian.cpp
@@ -0,0 +1,4 @@
+#include "Librarian.h"
+
+Librarian::Librarian(int _ID, int _password, string _name)
+	: Person(_ID, _password, _name) { }
---
+++ Library/Librarian.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Person.h"
+
+class Librarian : public Person
+{
+	Librarian(int _ID, int _password, string _name);
+};
+
